1c03dda30a0014d9accbd8eedc0296a17702d6d0
Yash Wairagade <theyashwairagade@gmail.com>
2025-03-04 09:05:41 +0530

    update 4

--- minHeap.cpp
+++
@@ -1,7 +0,0 @@
-#include<queue>
-#include<vector>
-
-priority_queue<int, vector<int>, greater<int>> minHeap;
-
-minHeap.push(5);
-minHeap.pop();
--- resetBoolVec.cpp
+++
@@ -1,5 +0,0 @@
-void reset(vector<bool> &vec) {
-    for (size_t i = 0; i < vec.size(); ++i) {
-        vec[i] = false;
-    }
-}
